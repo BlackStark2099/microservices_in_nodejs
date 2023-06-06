fb5fa265a7e241b612781a84ea814fac3408fde9
BlackStark2099 <manan9540608841@gmail.com>
2023-06-06 19:22:25 +0530

    commit

--- project_2/ticketing/auth/src/middlewares/error-handling.ts
+++ project_2/ticketing/auth/src/middlewares/error-handling.ts
@@ -4,6 +4,10 @@ import {Request, Response, NextFunction } from 'express';
 export const errorHandler = (
     err : Error,
     req : Request,
-) => {
-
-}
+    res : Response,
+    next : NextFunction
+    ) => {
+        res.status(400).send({
+           "message" : "something went wrong"
+        });    
+    };  
